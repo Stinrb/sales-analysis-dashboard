33f24fa412f695dd0cd11c1cd409d1e8d168ff7e
Justin <justinrfb@gmail.com>
2025-03-01 14:27:31 +0800

    Update sales_trend.sql

--- sales_trend.sql
+++ sales_trend.sql
@@ -2,9 +2,10 @@
 -- Sales Performance (2016-2018)
 -- ================================
 
--- Revenue and Quantity Sold Grouped by Year (2016-2018)
+-- Revenue, Quantity Sold, and Total Orders Grouped by Year (2016-2018)
 SELECT 
     EXTRACT(YEAR FROM so.time_order_taken) AS year,
+    COUNT(DISTINCT so.sales_order_id) AS total_orders,
     SUM(si.quantity) AS total_quantity,
     SUM(si.quantity * it.price) AS total_revenue
 FROM sales_order AS so
@@ -14,30 +15,21 @@ JOIN item AS it
     ON si.item_id = it.item_id
 JOIN product AS pr
         ON it.product_id = pr.product_id
-WHERE it.item_id NOT IN (28, 3, 50) OR pr.product_id NOT IN (13)
 GROUP BY year
 ORDER BY year ASC;
 
--- Overall Revenue and Quantity Sold (2016-2018)
-WITH sales_summary AS (
-    SELECT
-        si.item_id,
-        it.product_id,
-        pr.name,
-        SUM(si.quantity) AS total_quantity_sold,
-        SUM(si.quantity * it.price) AS total_revenue
-    FROM sales_item AS si
-    JOIN item AS it 
-        ON si.item_id = it.item_id
-    JOIN product AS pr
-        ON it.product_id = pr.product_id
-    WHERE it.item_id NOT IN (28, 3, 50) OR pr.product_id NOT IN (13)
-    GROUP BY pr.name, si.item_id, it.product_id
-)
+-- Overall Revenue, Quantity Sold, and Total Orders (2016-2018)
 SELECT
-    SUM(total_quantity_sold) AS total_quantity_sold,
-    SUM(total_revenue) AS total_revenue
-FROM sales_summary;
+    SUM(si.quantity) AS total_quantity_sold,
+    SUM(si.quantity * it.price) AS total_revenue,
+    (SELECT COUNT(DISTINCT so.sales_order_id)
+     FROM sales_order AS so
+        JOIN sales_item AS si
+            ON so.sales_order_id = si.sales_order_id
+    ) AS total_orders
+FROM sales_item AS si
+JOIN item AS it
+    ON si.item_id = it.item_id;
 
 -- Earliset and Latest Order Date (January 2016 - December 2018)
 SELECT
@@ -58,7 +50,6 @@ JOIN item AS it
     ON si.item_id = it.item_id
 JOIN product AS pr
         ON it.product_id = pr.product_id
-    WHERE it.item_id NOT IN (28, 3, 50) OR pr.product_id NOT IN (13)
 GROUP BY quarter, year
 ORDER BY year ASC, quarter ASC;
 
@@ -71,8 +62,5 @@ JOIN item AS it
     ON pr.product_id = it.product_id
 JOIN sales_item AS si
     ON it.item_id = si.item_id
-WHERE it.item_id NOT IN (28, 3, 50) OR pr.product_id NOT IN (13) -- Excluding certain items and products (discontinued or not sold)
 GROUP BY pr.name
 ORDER BY total_revenue DESC;
-
-
